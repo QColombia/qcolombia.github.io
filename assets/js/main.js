de5c21b00b1ac34fd6e7a3b90b84cb1f2e251e30
Diego Herrera Rojas <diegoherrerarojas@Diegos-MacBook-Air.local>
2021-12-20 16:05:59 -0500

    feature: set up functional navbar.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
-let a = 1;
-console.log("Hola! ", a);
+let activeDropdown = false;
+
+triggerDropdown = () => {
+    const menu = document.getElementById("dropdown-menu");
+    const trigger = document.getElementById("dropdown-trigger-icon");
+    if (!activeDropdown) {
+        menu.style.opacity="1";
+        menu.style.visibility="visible";
+        trigger.classList.remove("fa-bars")
+        trigger.classList.add("fa-times")
+    }
+    else {
+        menu.style.opacity="0";
+        menu.style.visibility="hidden";
+        trigger.classList.remove("fa-times")
+        trigger.classList.add("fa-bars")
+    }
+    activeDropdown = !activeDropdown;
+}
