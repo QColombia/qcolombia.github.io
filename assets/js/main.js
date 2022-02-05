ed418b6b4d369dd78e030c368786a89fd9a46b89
diegoherrera262 <diegoherrera262@gmail.com>
2022-02-04 19:38:46 -0500

    feature: set up home aspects section

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,3 +129,19 @@ navButtons.addEventListener("click", e => {
     moveToSlide(track, currentSlide, targetSlide);
     updateDots(currentDot, targetDot);
 })
+
+/*
+HOME ASPECTS CAROUSEL MANAGEMENT
+*/
+
+moveAspectsCarousel = (event) => {
+    const aspectsTrack = document.querySelector(".home-aspects__aspect-container");
+    const aspectsWindow = document.querySelector(".home-aspects__aspect-window");
+    // get target slide
+    const targetSlide = parseInt(event.dataset.itemId);
+    // Check if screen is large
+    const windowLength = aspectsWindow.offsetWidth;
+    if (windowLength > 300) return;
+    // translate aspects track
+    aspectsTrack.style.transform = "translate(-" + (targetSlide * 300).toString() + "px)";
+}
